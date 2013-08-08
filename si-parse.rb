0d78d66b3af720eba295cf3b2ea45363b60871e4
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-08 18:39:57 -0400

    Read each attribute field for its header and values

--- si-parse.rb
+++ si-parse.rb
@@ -24,6 +24,17 @@ csv_out = CSV.open("output.csv","w")
 ######### Loop through records #########
 html_records.each_with_index do |record, index|
 
+
+	# Check which fields are available
+	record.css("dl").each do |attribute|
+		col_header = getContent(attribute.at_css("dt")).delete(":")
+		print "#{col_header}: "
+		col_values = attribute.css("dd").each do |value|
+			print "#{getContent(value)}, "
+		end
+		puts
+	end	
+
 	# Increment progress bar
 	ProgressBar.create(:title => "Records processed", :starting_at => index, :total => num_records, :format => '|%b>>%i| %p%% %t', :throttle_rate => 0.2)
 end

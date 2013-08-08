7f73cfa7d7884f92ff351d173e977ae3d1c9a95f
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-08 18:19:19 -0400

    Basic parsing script that loads and loops through HTML

---
+++ si-parse.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'csv'
+require 'ruby-progressbar'
+
+
+######### Method to avoid exceptions when getting the content of empty nodes using Nokogiri #########
+def getContent(input)
+	if input == nil
+		return nil
+	else
+		return input.content
+	end
+end
+
+######### Initialize files #########
+print "Loading scraped HTML..."
+html_records = Nokogiri::HTML(open("output.html")).css("div.record")
+num_records = html_records.count
+puts "#{num_records} records loaded."
+
+puts "Initializing CSV"
+csv_out = CSV.open("output.csv","w")
+
+######### Loop through records #########
+html_records.each_with_index do |record, index|
+
+	# Increment progress bar
+	ProgressBar.create(:title => "Records processed", :starting_at => index, :total => num_records, :format => '|%b>>%i| %p%% %t', :throttle_rate => 0.2)
+end
+
+puts "Finished."

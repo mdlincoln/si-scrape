0d02f02746f1f7c222cd4283091be5834910a2dd
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-08 19:05:02 -0400

    Basic looping through each value in the field

--- si-parse.rb
+++ si-parse.rb
@@ -24,16 +24,20 @@ csv_out = CSV.open("output.csv","w")
 ######### Loop through records #########
 html_records.each_with_index do |record, index|
 
-
-	# Check which fields are available
+	# Loop through every field in the record
+	row = Array.new
 	record.css("dl").each do |attribute|
 		col_header = getContent(attribute.at_css("dt")).delete(":")
-		print "#{col_header}: "
-		col_values = attribute.css("dd").each do |value|
-			print "#{getContent(value)}, "
+
+		# Loop through every value in the field
+		values = String.new
+		attribute.css("dd").each do |value|
+			values << "#{getContent(value)}|"
 		end
-		puts
-	end	
+		row << values
+	end
+
+	csv_out << row
 
 	# Increment progress bar
 	ProgressBar.create(:title => "Records processed", :starting_at => index+1, :total => num_records, :format => '|%b>>%i| %p%% %t')
